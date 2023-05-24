a62f75f37413c0ad5c1d95160648b434a24b8a1c
Luis Reyes Rayas <luis.rayas@albarran.com.mx>
2023-05-24 10:23:23 -0600

    Se completa los transpasos de ventas web

--- app/Utils/Traits/AlmacenTrait.php
+++ app/Utils/Traits/AlmacenTrait.php
@@ -64,21 +64,24 @@ trait AlmacenTrait
         try {
             DB::beginTransaction();
             $idSucursalSolicita = ($idSucursalSolicita == null ? Auth::user()->id_sucursal : $idSucursalSolicita);
+            $contenido = collect($contenido);
 
             $indice = 0;
 
-            dd(Carbon::now());
-
             $traspaso = (object) [
+                'id' => null,
                 'traspaso' => '',
-                'contenido' => array()
+                'contenido' => collect()
             ];
 
             $numero = DB::table('a_numeracion')
-            ->selectRaw('CASE WHEN actual IS NULL THEN 1 ELSE actual + 1 END AS numeracion')
-            ->where('id_sucursal', $idSucursalEnvia)
+            ->selectRaw('CASE WHEN actual IS NULL THEN 1 ELSE actual END AS numeracion')
+            ->where('id_sucursal', $idSucursalSolicita)
+            ->where('id_numeracion_tipo', 7)
             ->where('vivo', true)
             ->first();
+
+            $numero->numeracion = $numero->numeracion + 1;
 
             //Solicita + Envia + Recibe + Numeracion actual
             $traspaso->traspaso = ''
@@ -88,23 +91,27 @@ trait AlmacenTrait
             . $numero->numeracion;
 
             foreach ($contenido as $producto) {
+                $indice = $indice + 1;
                 $costo = DB::table('c_precio')
                 ->select('precio')
                 ->where('id_producto', $producto->idProducto)
                 ->where('vivo', true)
                 ->first();
 
-                $id = DB::table('f_traspaso_contenido')
-                ->insertGetId([
+                $traspasoContenido = [
                     'vivo' => true,
                     'traspaso' => $traspaso->traspaso,
-                    'numero' => $indice++,
+                    'numero' => $indice,
                     'id_producto' => $producto->idProducto,
                     'cantidad' => $producto->cantidad,
                     'costo_unitario' => $costo->precio,
                     'costo_total' => floatval($costo->precio) * $producto->cantidad
-                ]);
-                array_push($traspaso->contenido, $id);
+                ];
+
+                $id = DB::table('f_traspaso_contenido')->insertGetId($traspasoContenido);
+                $traspasoContenido['id'] = $id;
+
+                $traspaso->contenido->push((object) $traspasoContenido);
 
                 $kardex = DB::table('f_almacen_kardex')
                 ->select('id', 'numero_movimiento', 'existencia_final')
@@ -124,11 +131,43 @@ trait AlmacenTrait
                     'numero_movimiento' => $kardex->numero_movimiento,
                     'existencia_final' => $kardex->existencia_final,
                     'id_almacen_movimiento' => 8, //"Traspaso Salida"
-                    'fh_algo' => Carbon::now()
+                    'fh_movimiento' => Carbon::now(),
+                    'referencia' => $traspaso->traspaso,
+                    'id_almacen_kardex_proveedor' => 2, //id_kardex_proveedor // id_almacen_nombre
+                    'iproveedor' => $idSucursalEnvia
                 ]);
             }
-            dump($traspaso);
-            DB::rollBack();
+
+            $traspaso->id = DB::table('f_traspaso')->insertGetId([
+                'id_sucursal_envia' => $idSucursalEnvia,
+                'id_sucursal_recive' => $idSucursalRecibe,
+                'id_usuario_envia' => Auth::user()->id,
+                'id_usuario_recive' => Auth::user()->id,
+                'fh_envio' => Carbon::now(),
+                'cantidad_envio' => $contenido->sum('cantidad'),
+                'traspaso' => $traspaso->traspaso,
+                'total' => $traspaso->contenido->sum('costo_total'),
+                'ck_solicitado' => false,
+                'ck_enviado' => true,
+                'id_transporte' => 1,
+                'id_transporte_chofer' => 10001, //id_operador = Albarran
+                'id_sucursal' => $idSucursalSolicita,
+                'costo' => 0,
+                'cantidad_servicio' => 1,
+                'comision' => 0,
+            ]);
+
+            DB::table('a_numeracion')
+            ->where('id_numeracion_tipo', 7)
+            ->where('id_sucursal', $idSucursalSolicita)
+            ->where('vivo', true)
+            ->update([
+                'actual' => $numero->numeracion
+            ]);
+
+
+            // dump($traspaso);
+            DB::commit();
             return $traspaso;
         } catch (Exception $e){
             DB::rollBack();
